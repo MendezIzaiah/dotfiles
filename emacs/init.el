0ce3d79aa3fdd9adc9256d19cc475bff99ef057d
Izaiah Mendez <24627848+MendezIzaiah@users.noreply.github.com>
2024-01-25 01:58:08 -0800

    added init

---
+++ emacs/init.el
@@ -0,0 +1,35 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(unless (package-installed-p 'spacemacs-theme)
+  (package-refresh-contents)
+  (package-install 'spacemacs-theme))
+;;;
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(setq org-src-window-setup 'current-window)
+;;(global-set-key (kbd "<s-return>") 'ansi-term)
+;;;;;
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(spacemacs-dark))
+ '(custom-safe-themes
+   '("7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" default))
+ '(package-selected-packages
+   '(spaceline avy org-tempo org-bullets evil-collection evil spacemacs-theme which-key use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "ADBO" :family "Source Code Pro")))))
